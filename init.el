6e1b954eb22048f324ae4b66b0372d8e4e0ac6d8
Max Kochurov <maxim.v.kochurov@gmail.com>
2024-04-24 15:12:55 +0300

    set up nix mode and auto highlighting with treesit auto

--- init.el
+++ init.el
@@ -2,4 +2,10 @@
 (add-to-list 'default-frame-alist '(font . "IBM Plex Mono" ))
 (set-face-attribute 'default t :font "IBM Plex Mono")
 (load-theme `modus-operandi)
-
+(setq-default indent-tabs-mode nil)
+(use-package nix-ts-mode
+ :mode "\\.nix\\'")
+(use-package treesit-auto
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
