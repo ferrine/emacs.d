4758ad889eb7c5dd81576338a50aa20edd07f304
Max Kochurov <maxim.v.kochurov@gmail.com>
2024-04-24 21:06:20 +0300

    use helm buffer list

--- init.el
+++ init.el
@@ -16,5 +16,6 @@
 (use-package helm
   :config
   (global-set-key (kbd "M-x") 'helm-M-x)
-  (global-set-key (kbd "C-x C-f") 'helm-find-files))
+  (global-set-key (kbd "C-x C-f") 'helm-find-files)
+  (global-set-key (kbd "C-x b") 'helm-buffers-list))
 
